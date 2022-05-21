5de0ba094e9e17b1fbabafec6732ce1d8771ab75
Hristo Shirov <hristo.shirov@mentormate.com>
2022-05-21 16:24:22 +0300

    Add SinglePostPage

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 import { Navbar } from './app/Navbar';
 import PostList from './features/posts/PostsList';
 import AddPostForm from './features/posts/AddPostForm';
+import SinglePostPage from './features/posts/SinglePostPage';
 
 const App = () => {
   return (
@@ -26,6 +27,7 @@ const App = () => {
               </>
             )}
           />
+          <Route exact path='/posts/:postId' component={SinglePostPage} />
           <Redirect to="/" />
         </Switch>
       </div>
